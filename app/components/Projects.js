404ec2011ad63972eb2a480557858956a3ea1499
lokendra-cmd <lokendra69.lk@gmail.com>
2025-03-29 23:19:54 +0530

    update

--- app/components/Projects.js
+++ app/components/Projects.js
@@ -49,7 +49,7 @@ const Projects = () => {
 
                     </div>
                     <div className="buttons ">
-                        <button onClick={() => openVideoModal('HEEibYnZw_s')} className="btn">Watch Demo</button>
+                        <button onClick={() => openVideoModal('')} className="btn">Watch Demo</button>
                         <button onClick={() => window.open("https://github.com/lokendra-cmd/inspect", "_blank")} className="btn">GitHub</button>
                     </div>
                 </div>
